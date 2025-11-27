d812a75fbf5b85a9eca773777a9261b827d462ca
MarlonHD <marlonsilva840@gmail.com>
2025-11-27 12:17:08 -0300

    feat - aula sobre ângulos com servomotor

---
+++ Matemática/Angulos/servomotor.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+int pos = 0;
+
+Servo servo;
+
+void setup()
+{
+  servo.attach(9, 500, 2500);
+}
+
+void loop()
+{
+  for (pos = 0; pos <= 180; pos += 1) {
+    servo.write(pos);
+    delay(15); 
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+    servo.write(pos);
+    delay(15); 
+  }
+}
